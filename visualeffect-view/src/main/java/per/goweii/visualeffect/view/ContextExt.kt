eeb8453ccb87097b255be8d26fb2205fe7de50fa
goweii <goweii@163.com>
2021-08-19 20:06:48 +0800

    移除自定义rs，回退minsdk14

--- visualeffect-view/src/main/java/per/goweii/visualeffect/view/ContextExt.kt
+++ visualeffect-view/src/main/java/per/goweii/visualeffect/view/ContextExt.kt
@@ -5,14 +5,18 @@ import android.content.Context
 import android.content.ContextWrapper
 
 fun Context.getActivity(): Activity? {
-    if (this is Activity) {
-        return this
-    }
-    if (this is ContextWrapper) {
-        val ctx = this.baseContext
-        if (ctx !== this) {
-            return ctx.getActivity()
+    var context = this
+    while (true) {
+        if (context is Activity) {
+            return context
         }
+        if (context is ContextWrapper) {
+            val baseContext = context.baseContext
+            if (baseContext !== context) {
+                context = baseContext
+                continue
+            }
+        }
+        return null
     }
-    return null
 }
