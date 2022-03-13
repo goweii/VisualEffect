871e8d46c2f97925a850651b204e4b072af53da1
goweii <goweii@163.com>
2022-03-13 09:35:27 +0800

    refactor: 修改parcelable自定义

--- visualeffect-core/src/main/java/per/goweii/visualeffect/core/ParcelableVisualEffect.kt
+++ visualeffect-core/src/main/java/per/goweii/visualeffect/core/ParcelableVisualEffect.kt
@@ -3,9 +3,8 @@ package per.goweii.visualeffect.core
 import android.content.Context
 import android.os.Parcel
 import android.os.Parcelable
-import java.lang.Exception
 
-abstract class ParcelableVisualEffect: VisualEffect, Parcelable {
+abstract class ParcelableVisualEffect : VisualEffect, Parcelable {
     open fun readFromParcel(parcel: Parcel) {
     }
 
@@ -17,38 +16,41 @@ abstract class ParcelableVisualEffect: VisualEffect, Parcelable {
         return 0
     }
 
-    companion object CREATOR : Parcelable.Creator<ParcelableVisualEffect> {
-        override fun createFromParcel(parcel: Parcel): ParcelableVisualEffect? {
-            val className = parcel.readString()
-            if (className.isNullOrEmpty()) return null
-            val visualEffect = createVisualEffectByClassName(className) ?: return null
-            visualEffect.readFromParcel(parcel)
-            return visualEffect
-        }
+    companion object {
+        @JvmField
+        val CREATOR = object : Parcelable.Creator<ParcelableVisualEffect> {
+            override fun createFromParcel(parcel: Parcel): ParcelableVisualEffect? {
+                val className = parcel.readString()
+                if (className.isNullOrEmpty()) return null
+                val visualEffect = createVisualEffectByClassName(className) ?: return null
+                visualEffect.readFromParcel(parcel)
+                return visualEffect
+            }
 
-        override fun newArray(size: Int): Array<ParcelableVisualEffect?> {
-            return arrayOfNulls(size)
-        }
+            override fun newArray(size: Int): Array<ParcelableVisualEffect?> {
+                return arrayOfNulls(size)
+            }
 
-        private fun createVisualEffectByClassName(className: String?): ParcelableVisualEffect? {
-            className ?: return null
-            var cls: Class<*>? = null
-            try {
-                cls = Class.forName(className)
-            } catch (e: Exception) {
+            private fun createVisualEffectByClassName(className: String?): ParcelableVisualEffect? {
+                className ?: return null
+                var cls: Class<*>? = null
+                try {
+                    cls = Class.forName(className)
+                } catch (e: Exception) {
+                }
+                cls ?: return null
+                try {
+                    val c = cls.getConstructor()
+                    return c.newInstance() as ParcelableVisualEffect
+                } catch (e: Exception) {
+                }
+                try {
+                    val c = cls.getConstructor(Context::class.java)
+                    return c.newInstance() as ParcelableVisualEffect
+                } catch (e: Exception) {
+                }
+                return null
             }
-            cls ?: return null
-            try {
-                val c = cls.getConstructor()
-                return c.newInstance() as ParcelableVisualEffect
-            } catch (e: Exception) {
-            }
-            try {
-                val c = cls.getConstructor(Context::class.java)
-                return c.newInstance() as ParcelableVisualEffect
-            } catch (e: Exception) {
-            }
-            return null
         }
     }
 
